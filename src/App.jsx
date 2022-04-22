10a6f77bf27014a24c6cb4a990d4cf37e8853aab
MadhanM282 <madhanm282@gmail.com>
2022-04-22 15:43:52 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+// import './App.css'
 import { SignupSignin } from './components/SignupSignIn/signin'
 import { Route, Routes } from 'react-router'
 import ResponsiveAppBar from './components/Navbar/Navbar';
