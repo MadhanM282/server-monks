5e5e3df7a35accbff12f10bf931e29b30d737696
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-22 23:29:59 +0530

    routes and redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -29,5 +29,3 @@ const rootReducer = combineReducers({
 export const store = createStore(rootReducer, enhancer);
 
 
-
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
