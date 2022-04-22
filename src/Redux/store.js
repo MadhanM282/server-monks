d0084b6904e0564d40e9630007f386dd79158151
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-22 23:25:46 +0530

    routes and redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,23 @@
 import { composeWithDevTools } from "redux-devtools-extension";
-import { combineReducers, createStore } from "redux";
+import { combineReducers, compose, createStore } from "redux";
 import { applyMiddleware } from "redux";
 import { AuthReducer } from "./auth/authReducer";
 import thunk from "redux-thunk";
 import { clubHomeReducer } from "./Home/clubHomeReducer";
 import { registerReducer } from "./register/registerReducer";
+
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__({
+  
+  }) : compose;
+
+const middleware = [thunk];
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+);
 
 
 const rootReducer = combineReducers({
@@ -13,8 +26,7 @@ const rootReducer = combineReducers({
   club: clubHomeReducer
 });
 
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, enhancer);
 
 
 
