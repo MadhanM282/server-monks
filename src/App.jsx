5bd65f8ef9d56cd5d943f18e3c887d684ef70b9f
Jibin Babu <jibinbabu966@gmail.com>
2022-04-22 15:43:13 +0530

    css bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-
-import './App.css'
-import { SignupSignin } from './components/SignupSignIn/signin'
-import { Route, Routes } from 'react-router'
-import ResponsiveAppBar from './components/Navbar/Navbar';
+// import './App.css'
+import { SignupSignin } from "./components/SignupSignIn/signin";
+import { Route, Routes } from "react-router";
+import { Home } from "./components/Home/home";
+import ResponsiveAppBar from "./components/Navbar/Navbar";
 
 function App() {
-  
-
   return (
     <div className="App">
-      <ResponsiveAppBar/>
+      <ResponsiveAppBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignupSignin />} />
       </Routes>
     </div>
   );
 }
-
-
 
 export default App;
---
+++ src/Redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+const middleware = [thunk];
+export const store = createStore(
+  "reducer add here",
+  composeWithDevTools(applyMiddleware(...middleware))
+);
