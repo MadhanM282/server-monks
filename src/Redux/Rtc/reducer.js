35cfefaa03d2a7be82b49ae45c2c630a991df7c2
MadhanM282 <madhanm282@gmail.com>
2022-04-23 11:53:26 +0530

    RTC

--- src/Redux/Rtc/reducer.js
+++ src/Redux/Rtc/reducer.js
@@ -1,9 +1,14 @@
-import { USER } from "./actions";
+import { CLUBID, USER } from "./actions";
 
-// const initial
+const initial = {
+    user:{},
+    ClubId:""
+}
 
-export const RwtReducer = (store,{type,payload}) =>{
+export const RwtReducer = (store=initial,{type,payload}) =>{
     switch (type) {
-        case USER: return {}
+        case USER: return {...store,user: payload}
+        case CLUBID : return{...store,ClubId: payload}
+        default: return{store} 
     }
 }
