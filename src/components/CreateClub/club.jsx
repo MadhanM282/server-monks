f00cfb042c17161f2d406255df28bfb72d00ccc3
Vaibhav Upasani <upasanivaibhav7@gmail.com>
2022-04-23 09:23:24 +0530

    added form

--- src/components/CreateClub/club.jsx
+++ src/components/CreateClub/club.jsx
@@ -1,59 +1,86 @@
 import { useState } from 'react';
 import './club.css';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { nanoid } from 'nanoid';
+import { clubListData } from '../../Redux/Home/clubHomeAction';
 
 
 
 export const Club = () => {
-  const [clubData,changeData]=useState({
-    club_title:"",
-    image:"",
-    type:"",
-    description:"",
-    creator_id:""
+  const { user } = useSelector((store) => store.auth),dispatch=useDispatch();
+  const navigate=useNavigate();
+  // console.log(user);
+  const [clubData, changeData] = useState({
+    club_title: "",
+    image: "",
+    type: "",
+    description: "",
+    creator_id: user
 
   })
- 
+  console.log(clubData);
+
+
 
   const handleChange = (e) => {
     // console.log(title, desc, cat);
     // alert("Data added succesfully");
     // navigate('/');
+    // console.log(e, e.target.value);
+    const { id, value } = e.target;
+    changeData({...clubData,[id]: value });
+  
 
   }
-  const submitData=(e)=>{
+  const submitData = (e) => {
     e.preventDefault();
+    // dispatch(clubListData(clubData,navigate))
+    console.log("The data is",clubData);
 
   }
 
   const options = [
     {
       value: "grouping",
-      label: "Grouping"
+      label: "Grouping",
+      id: nanoid()
+
+
     },
     {
       value: "dressing",
-      label: "Dressing"
+      label: "Dressing",
+      id: nanoid()
+
     },
     {
       value: "inspiration",
-      label: "Inspiration"
+      label: "Inspiration",
+      id: nanoid()
+
     },
     {
       value: "games",
-      label: "Games"
+      label: "Games",
+      id: nanoid()
+
+
     },
     {
       value: 'exam',
-      label: 'Exam'
+      label: 'Exam',
+      id: nanoid()
     },
     {
       value: "study_rooms",
-      label: "Study_rooms"
+      label: "Study_rooms",
+      id: nanoid()
     },
     {
       value: "coding",
-      label: "Coding"
+      label: "Coding",
+      id: nanoid()
     }
   ]
 
@@ -61,10 +88,24 @@ export const Club = () => {
     <>
       <div id='contain'>
         <form action="" onSubmit={submitData}>
-          <input type="text" placeholder='Enter the title...' id='club_title' 
-          onChange={handleChange}/>
-
-         
+          <label htmlFor="">Title</label> <br />
+          <input type="text" placeholder='Enter the title...' id='club_title' required
+            onChange={handleChange} /> <br />
+          <label htmlFor="">Select image</label>
+          <input type="file" placeholder='choose image....' id='image' required
+            accept='image/*' onChange={handleChange} /> <br />
+          <label htmlFor="">Type</label>
+          <select name="" id="type" onChange={handleChange}>
+            {options.map((e) => (
+              <option value={e.value} key={e.id} id='type'>{e.label}</option>
+            ))}
+          </select> <br />
+          <label htmlFor="">Description</label>
+          <textarea name="" id="description" cols="30" rows="10"
+            placeholder='Enter the description...'
+            onChange={handleChange}
+          ></textarea> <br />
+          <button>Create Club</button>
         </form>
 
       </div>
