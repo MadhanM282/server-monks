face911b7ed01b0035c00ff86bd92bdfb5c9a897
Jibin Babu <jibinbabu966@gmail.com>
2022-04-23 15:24:05 +0530

    devtools

--- src/Redux/Rtc/reducer.js
+++ src/Redux/Rtc/reducer.js
@@ -1,14 +1,17 @@
 import { CLUBID, USER } from "./actions";
 
 const initial = {
-    user:{},
-    Club:{}
-}
+  user: {},
+  Club: {},
+};
 
-export const RwtReducer = (store=initial,{type,payload}) =>{
-    switch (type) {
-        case USER: return {...store,user: payload}
-        case CLUBID : return{...store,Club: payload}
-        default: return{store} 
-    }
-}
+export const RwtReducer = (store = initial, { type, payload }) => {
+  switch (type) {
+    case USER:
+      return { ...store, user: payload };
+    case CLUBID:
+      return { ...store, Club: payload };
+    default:
+      return { store };
+  }
+};
--- src/components/SignupSignIn/signin.jsx
+++ src/components/SignupSignIn/signin.jsx
@@ -1,120 +1,166 @@
-import axios from "axios"
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router"
-import { loginSuccessData } from "../../Redux/auth/authAction"
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { loginSuccessData } from "../../Redux/auth/authAction";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./style.css"
-import { registerSuccessData } from "../../Redux/register/registerAction"
-import { UserData } from "../../Redux/Rtc/actions"
-
-
+import "./style.css";
+import { registerSuccessData } from "../../Redux/register/registerAction";
+import { UserData } from "../../Redux/Rtc/actions";
 
 export const SignupSignin = () => {
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  const [Signup, SetSignup] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-    const navigate = useNavigate();
-    // const dispatch = useDispatch();
-    const [Signup,SetSignup] = useState({
-        firstName:"",
-        lastName:"",
-        email: "",
-        password: ""
-    })
+  const HandelChangeSignup = (e) => {
+    const { id, value } = e.target;
+    SetSignup({ ...Signup, [id]: value });
+  };
 
-    const HandelChangeSignup = (e)=>{
-        const {id,value} = e.target;
-        SetSignup({...Signup,[id]:value})
-    }
+  const [Signin, SetSignin] = useState({
+    email: "",
+    password: "",
+  });
 
+  const HandelChangeSignin = (e) => {
+    const { id, value } = e.target;
+    SetSignin({ ...Signin, [id]: value });
+  };
 
+  const Register = () => {
+    console.log("Signup", Signup);
+    // axios.post(`https://server-monks-backend.herokuapp.com/register`,Signup).then(({data})=>{
+    //     console.log('Register data', data);
+    // })
+    dispatch(registerSuccessData(Signup, toast));
+    container.classList.remove("right-panel-active");
+  };
 
-    const [Signin,SetSignin] = useState({
-        email: "",
-        password: ""
-    })
+  const Login = () => {
+    console.log("Signin", Signin);
+    dispatch(UserData(Signin));
+    dispatch(loginSuccessData(Signin, toast, navigate));
+    // axios.post(`https://server-monks-backend.herokuapp.com/login`,Signin).then(({data})=>{
+    //     console.log('Login data', data);
+    //     navigate("/")
 
-    
-    const HandelChangeSignin = (e)=>{
-        const {id,value} = e.target;
-        SetSignin({...Signin,[id]:value})
-    } 
-    
-    const Register = ()=>{
-        console.log('Signup', Signup);
-        // axios.post(`https://server-monks-backend.herokuapp.com/register`,Signup).then(({data})=>{
-        //     console.log('Register data', data);
-        // })
-        dispatch(registerSuccessData(Signup, toast))
-        container.classList.remove("right-panel-active")
-    }
-    
-    const Login = ()=>{
-        console.log('Signin', Signin);
-        dispatch(loginSuccessData(Signin,toast,navigate))
-        dispatch(UserData(Signin))
-        // axios.post(`https://server-monks-backend.herokuapp.com/login`,Signin).then(({data})=>{
-        //     console.log('Login data', data);
-        //     navigate("/")
-            
-        // })
-       
-    }
+    // })
+  };
 
-    return (
-        <>
-            <div className="container" id="container">
-                <div className="form-container sign-up-container">
-                    <form action="#">
-                        <h1>Create Account</h1>
-                        <input onChange={HandelChangeSignup} id="firstName" type="text" placeholder="First Name" />
-                        <input onChange={HandelChangeSignup} id="lastName" type="text" placeholder="Last Name" />
-                        <input onChange={HandelChangeSignup} id="email" type="email" placeholder="Email" />
-                        <input onChange={HandelChangeSignup} id="password" type="password" placeholder="Password" />
-                        <button onClick={(e) => {
-                            e.preventDefault()
-                            Register()
-                        }} > Sign Up</button>
-                    </form>
-                </div>
-                <div className="form-container sign-in-container">
-                    <form action="#">
-                        <h1>Sign in</h1>
-                        <input id="email" onChange={HandelChangeSignin} type="email" placeholder="Email" />
-                        <input id="password" onChange={HandelChangeSignin} type="password" placeholder="Password" />
-                        <a href="#">Forgot your password?</a>
-                        <button onClick={(e) => {
-                            e.preventDefault()
-                            Login()
-                        }} >Sign In</button>
-                    </form>
-                </div>
-                <div className="overlay-container">
-                    <div className="overlay">
-                        <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>To keep connected with us please login with your personal info</p>
-                            <button onClick={()=>{
-                                 container.classList.remove("right-panel-active")
-                            }} className="ghost" id="signIn">Sign In</button>
-                        </div>
-                        <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>Enter your personal details and start journey with us</p>
-                            <button onClick={()=>{
-                                 container.classList.add("right-panel-active")
-                            }} className="ghost" id="signUp">Sign Up</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container" id="container">
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h1>Create Account</h1>
+            <input
+              onChange={HandelChangeSignup}
+              id="firstName"
+              type="text"
+              placeholder="First Name"
+            />
+            <input
+              onChange={HandelChangeSignup}
+              id="lastName"
+              type="text"
+              placeholder="Last Name"
+            />
+            <input
+              onChange={HandelChangeSignup}
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+            <input
+              onChange={HandelChangeSignup}
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                Register();
+              }}
+            >
+              {" "}
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign in</h1>
+            <input
+              id="email"
+              onChange={HandelChangeSignin}
+              type="email"
+              placeholder="Email"
+            />
+            <input
+              id="password"
+              onChange={HandelChangeSignin}
+              type="password"
+              placeholder="Password"
+            />
+            <a href="#">Forgot your password?</a>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                Login();
+              }}
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                onClick={() => {
+                  container.classList.remove("right-panel-active");
+                }}
+                className="ghost"
+                id="signIn"
+              >
+                Sign In
+              </button>
             </div>
-            <ToastContainer />
-        </>
-    )
-}
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button
+                onClick={() => {
+                  container.classList.add("right-panel-active");
+                }}
+                className="ghost"
+                id="signUp"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
 
 /*
 
