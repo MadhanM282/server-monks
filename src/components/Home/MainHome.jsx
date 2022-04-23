bbe51f472409558c20c77fd7bc33a14b9471620c
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-24 01:56:36 +0530

    imgeslider

--- src/components/Home/MainHome.jsx
+++ src/components/Home/MainHome.jsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import ResponsiveAppBar from "../Navbar/Navbar";
 import { Home } from "./home";
+import { ImageSlider } from "./ImageSlider";
 export const MainHome = () => {
   return (
     <Box>
       <ResponsiveAppBar />
+      <ImageSlider/>
       <Home />
     </Box>
   );
