028c7c6e469d2cc7f80abb2a96b754b38f7eb965
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-23 23:44:36 +0530

    chnged

---
+++ src/Redux/suscribe/suscribeAction.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { clubError } from "../Home/clubHomeAction";
+
+export const UPDATE_CLUB = "UPDATE_CLUB";
+
+export const updateClubListt = (payload) => ({ type: UPDATE_CLUB, payload });
+
+
+
+export const UPDATE_USER = "UPDATE_USER";
+
+export const updateUserInfo = (payload) => ({ type: UPDATE_USER, payload });
+
+
+export const updateClubListDataa = (data, id, toast, navigate) => (dispatch) => {
+  
+    axios
+        .patch(`https://server-monks-backend.herokuapp.com/clubs/${id}`, data)
+        .then(({ data }) => {
+            
+            dispatch(updateClubListt(data));
+            toast.success("Suscribed Added!", {
+                position: "top-center",
+            });
+            setTimeout(() => {
+                navigate("/clubDetails");
+            }, 1000);
+        })
+        .catch((err) => {
+            dispatch(clubError());
+            toast.error("Try again!", {
+                position: "top-center",
+            });
+        });
+};
+
+export const updateUserInfoData = (data, id, toast, navigate) => (dispatch) => {
+   
+    axios
+        .patch(`https://server-monks-backend.herokuapp.com/users/${id}`, data)
+        .then(({ data }) => {
+            dispatch(updateUserInfo(data));
+            toast.success("Suscribed Added!", {
+                position: "top-center",
+            });
+            setTimeout(() => {
+                navigate("/clubDetails");
+            }, 1000);
+        })
+        .catch((err) => {
+            dispatch(clubError());
+            toast.error("Try again!", {
+                position: "top-center",
+            });
+        });
+};
