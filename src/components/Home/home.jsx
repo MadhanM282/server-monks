56fb66944d5b7bddcb12bd232e3ee0331bd386d7
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-23 11:22:10 +0530

    login action

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -13,6 +13,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { ClubCard } from "../Card/ClubCard";
 
 export const Home = () => {
 
@@ -115,7 +116,7 @@ export const Home = () => {
         }}
       >
         {clubList.clubs && clubList.clubs.map((event) => {
-          return <EventCard key={event._id} event={event} />;
+          return <ClubCard key={event._id} event={event} />;
         })}
       </div>
       <br />
